5766120c8c986e531993233c412ed928e77566af
Leonardo Scarparo de Miranda <leoscmiranda@gmail.com>
2023-03-14 22:16:30 -0300

    Realizado o exercício de operações em JavaScript

---
+++ aula02/exercicio_operacoes.js
@@ -0,0 +1,21 @@
+let a = 9
+let b = 3
+
+//Adição
+console.log("A soma será igual a:", a+b)
+
+//Subtração
+console.log("A diferença será igual a:", a-b)
+
+//Multiplicação
+console.log("O produto será igual a:", a*b)
+
+//Divisão
+console.log("O quociente será igual a:", a/b)
+
+//Potenciação
+console.log(a,"elevado a",b,"será igual a",a**b)
+
+//Radiciação
+console.log("\nA raiz quadrada de",a,"será igual a:", Math.sqrt(a)) //Raiz de a
+console.log("Já a raiz quadrada de",b,"será:", Math.sqrt(b)) //Raiz de b
