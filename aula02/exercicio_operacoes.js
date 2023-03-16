16fa496f3f84f57a307f7ef493038ba6b8b564b5
Leonardo Scarparo de Miranda <leoscmiranda@gmail.com>
2023-03-16 18:29:28 -0300

    Melhorado o exercício de operações

--- aula02/exercicio_operacoes.js
+++ aula02/exercicio_operacoes.js
@@ -2,20 +2,36 @@ let a = 9
 let b = 3
 
 //Adição
-console.log("A soma será igual a:", a+b)
+console.log("A soma (adição) será igual a:", a+b)
 
 //Subtração
-console.log("A diferença será igual a:", a-b)
+console.log("A diferença (subtração) será igual a:", a-b)
 
 //Multiplicação
-console.log("O produto será igual a:", a*b)
+console.log("O produto (multiplicação) será igual a:", a*b)
 
 //Divisão
-console.log("O quociente será igual a:", a/b)
+console.log("O quociente (divisão) será igual a:", a/b)
 
 //Potenciação
-console.log(a,"elevado a",b,"será igual a",a**b)
+console.log("\n"+a,"elevado a",b,"será igual a",a**b)
 
 //Radiciação
 console.log("\nA raiz quadrada de",a,"será igual a:", Math.sqrt(a)) //Raiz de a
 console.log("Já a raiz quadrada de",b,"será:", Math.sqrt(b)) //Raiz de b
+
+//Essa é a maneira mais simples de se fazer
+//Porém, em certos casos, pode ser melhor utilizar funções
+
+//Ficaria desse jeito:
+/*
+function Funcao_Adicao (a, b) { 
+    return a + b
+}
+
+console.log("Soma =", Funcao_Adicao(a, b))
+*/
+
+//"a" e "b" são parâmetros da função, que se comportam como variáveis locais
+//O código dentro da função é executado quando algo invoca(chama) ele
+//O "return" para a execução da função
